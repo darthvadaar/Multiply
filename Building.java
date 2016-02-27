f020eb9e91b90d1ab407afc58f98f8467c4e253c
Sid Bedekar <sid.bedekar@gmail.com>
2016-02-27 15:17:45 -0500

    Media files and image setup in Building.java

--- Building.java
+++ Building.java
@@ -1,10 +1,13 @@
 //Building class handles each building
+import java.awt.*;
+import javax.swing.*;
 
 public class Building{
 	public String type;
 	public boolean built;
 	public int population, x, y;
 	public World world;
+	Image image;
 	
 	public Building(String type, int x, int y, int population){
 		this.type = type;
@@ -12,6 +15,16 @@ public class Building{
 		this.y = y;
 		this.population = population;
 		this.built = false;
+		
+		if (type.equals("temple")){
+			image = new ImageIcon("temple.png").getImage();			
+		}
+		else if (type.equals("cave")){
+			image = new ImageIcon("cave.png").getImage();			
+		}
+		else if(type.equals("farm")){
+			image = new ImageIcon("farm.gif").getImage();
+		}
 		
 	}
 	
